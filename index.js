cdfa9a0a3a72bc70edfece75f0ed4dffad76516a
Hannah Brog <bugsbrog@Hannahs-MBP.home>
2021-09-13 14:08:37 -0600

    first push

--- index.js
+++ index.js
@@ -29,10 +29,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   1. What is the difference between counter1 and counter2?
   
+
+
   2. Which of the two uses a closure? How can you tell?
   
+
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+
+     
 */
 
 // counter1 code
